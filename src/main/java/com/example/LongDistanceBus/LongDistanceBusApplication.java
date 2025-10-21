9372af80945d225cdf94d9876711072ee33f4c56
DuongTrong35 <duongtrongtan352004@gmail.com>
2025-10-21 21:01:16 +0700

    Initial commit

---
+++ src/main/java/com/example/LongDistanceBus/LongDistanceBusApplication.java
@@ -0,0 +1,13 @@
+package com.example.LongDistanceBus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongDistanceBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LongDistanceBusApplication.class, args);
+	}
+
+}
